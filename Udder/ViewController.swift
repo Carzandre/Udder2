1bc9fc60b2e85293261354b5551b3475e523db6f
Carzandre <carzandre967@gmail.com>
2022-05-30 23:55:05 -0300

    acrescentado view will disappear.

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -17,5 +17,12 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         
     }
-}
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        
+    }
+    }
+        
+    }
+
 
